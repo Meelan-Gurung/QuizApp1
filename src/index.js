0d024389729d9b95e8ce4f0e8298c130cea2dba1
MT-creator <grgmilan1999@gmail.com>
2021-11-06 19:21:49 +0545

    quiz app try

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assests/style.css";
+import Q from "./Q";
+import QuestionBox from "./component/QuestionBox";
+import Result from "./component/Result";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Quiz extends React.Component {
+    state = {
+        questionBank: [],
+        score: 0,
+        responses: 0
+    };
+    getQuestions = () => {
+        Q().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    componentDidMount() {
+        this.getQuestions();
+    }
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    };
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        });
+    };
+    render() {
+        return (<div className="container" >
+            <div className="title" > Quiz </div> {
+                this.state.questionBank.length > 0 &&
+                this.state.responses < 5 &&
+                this.state.questionBank.map(
+                    ({ question, answers, correct, questionId }) => (
+                        <QuestionBox question={question}
+                            options={answers}
+                            key={questionId}
+                            selected={answer => this.computeAnswer(answer, correct)} />
+                    )
+                )}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
+        </div>
+        );
+    }
+};
+
+ReactDOM.render(< Quiz />, document.getElementById("root"));
